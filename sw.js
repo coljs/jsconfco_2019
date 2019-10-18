314505915dadcf4065306c4b8f027a189025b9cb
Jesse Cogllo <cogollo87@gmail.com>
2019-10-18 10:25:46 -0500

    fix skillshare

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2f78a4397c0b6c4e3def.js"
+    "url": "webpack-runtime-e1534ccf214ed38164ee.js"
   },
   {
     "url": "app-d4378bde6ad9fc727490.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "655660766c9f7c16899d9853b2d5843d"
+    "revision": "c99ae1819202fc49c54573a844966335"
   },
   {
     "url": "0.379d95077c12a8ad8e75.css"
